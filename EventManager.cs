df3aca83668849067d63fb1c71e62a599a2d8cae
Nemowang <211108100@smail.nju.edu.cn>
2025-07-13 17:46:20 +0800

    feat: implement EventManager and MapManager for hex grid interactions

---
+++ EventManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HexcellsHelper {
+
+    public static class EventManager
+    {
+
+        public delegate void LevelLoadedEventHandler();
+        public static event LevelLoadedEventHandler LevelLoaded;
+
+        public delegate void HexBehaviourEventHandler(HexBehaviour instance);
+        public static event HexBehaviourEventHandler DestroyClicked;
+        public static event HexBehaviourEventHandler HighlightClicked;
+
+        public static void Init()
+        {
+            SceneManager.sceneLoaded += (_, _) => OnLevelLoaded();
+        }
+
+        public static void OnLevelLoaded()
+        {
+            var hexGrid = GameObject.Find("Hex Grid");
+            var hexGridOverlay = GameObject.Find("Hex Grid Overlay");
+            if (hexGrid == null || hexGridOverlay == null)
+            {
+                return;
+            }
+            LevelLoaded?.Invoke();
+        }
+
+        public static void OnDestroyClick(HexBehaviour instance)
+        {
+            DestroyClicked?.Invoke(instance);
+        }
+
+        public static void OnHighlightClick(HexBehaviour instance)
+        {
+            HighlightClicked?.Invoke(instance);
+        }
+    }
+}
---
+++ MapManager.cs
@@ -0,0 +1,136 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+namespace HexcellsHelper
+{
+
+    public static class MapManager
+    {
+        public static GameObject[,] grid;
+        public static GameObject[,] gridOverlay;
+        public static MusicDirector musicDirector;
+        static GameObject hexGrid;
+        static GameObject hexGridOverlay;
+        static EditorFunctions editorFunctions;
+        static HexScoring score;
+        static TextMesh remainingText;
+
+        public static void Init()
+        {
+            EventManager.LevelLoaded += InitializeMap;
+            EventManager.DestroyClicked += UpdateMap;
+            EventManager.HighlightClicked += UpdateMap;
+        }
+
+        public static bool Hidden(int x, int y)
+        {
+            return gridOverlay[x, y] != null;
+        }
+        public static bool Hidden(Coordinate coordinate)
+        {
+            return Hidden(coordinate.x, coordinate.y);
+        }
+
+        public static void SetBlack(int x, int y)
+        {
+            // No need to update gridOverlay.
+            // Because UpdateMap is registered when DestroyClick is called.
+            gridOverlay[x, y]?.transform.GetComponent<HexBehaviour>().DestroyClick();
+        }
+
+        public static void SetBlack(Coordinate coordinate)
+        {
+            SetBlack(coordinate.x, coordinate.y);
+        }
+
+        public static void SetBlue(int x, int y)
+        {
+            // No need to update gridOverlay.
+            // Because UpdateMap is registered when HighlightClick is called.
+            gridOverlay[x, y]?.transform.GetComponent<HexBehaviour>().HighlightClick();
+        }
+
+        public static void SetBlue(Coordinate coordinate)
+        {
+            SetBlue(coordinate.x, coordinate.y);
+        }
+
+        public static bool SetYellow(int x, int y)
+        {
+            if (grid[x, y] == null || gridOverlay[x, y] != null)
+            {
+                return false;
+            }
+            gridOverlay[x, y] = Object.Instantiate(
+                editorFunctions.orangeHex,
+                grid[x, y].transform.position,
+                editorFunctions.orangeHex.transform.rotation,
+                hexGridOverlay.transform
+            );
+            if (grid[x, y].tag == "Blue")
+            {
+                score.numberOfCorrectTilesFound--;
+                gridOverlay[x, y].GetComponent<HexBehaviour>().containsShapeBlock = true;
+                remainingText.text = (score.numberOfBlueTiles - score.numberOfCorrectTilesFound).ToString();
+                BepInEx.Bootstrap.Chainloader.ManagerObject
+                    .GetComponent<DisplayModeManager>()
+                    .UpdateHexNumbers(); 
+            }
+            score.tilesRemoved--;
+            musicDirector.PlayWrongNote(gridOverlay[x, y].transform.position.x / 7.04f);
+            iTween.ShakePosition(gridOverlay[x, y], new Vector3(0.1f, 0.1f, 0f), 0.3f);
+            return true;
+        }
+
+        public static bool SetYellow(Coordinate coordinate)
+        {
+            return SetYellow(coordinate.x, coordinate.y);
+        }
+
+        static void InitializeMap()
+        {
+            hexGrid = GameObject.Find("Hex Grid");
+            hexGridOverlay = GameObject.Find("Hex Grid Overlay");
+            editorFunctions = GameObject.Find("Editor Functions").GetComponent<EditorFunctions>();
+            score = GameObject.Find("Score Text").GetComponent<HexScoring>();
+            remainingText = score.GetComponent<TextMesh>();
+            musicDirector = GameObject.Find("Music Director(Clone)").GetComponent<MusicDirector>();
+
+            grid = new GameObject[CoordUtil.Width, CoordUtil.Height];
+            foreach (Transform tr in hexGrid.transform)
+            {
+                var coordinate = CoordUtil.WorldToGrid(tr.position);
+                if (!CoordUtil.IsValidCoord(coordinate))
+                {
+                    continue;
+                }
+                grid[coordinate.x, coordinate.y] = tr.gameObject;
+            }
+
+            gridOverlay = new GameObject[CoordUtil.Width, CoordUtil.Height];
+            foreach (Transform tr in hexGridOverlay.transform)
+            {
+                var coordinate = CoordUtil.WorldToGrid(tr.position);
+                if (!CoordUtil.IsValidCoord(coordinate))
+                {
+                    continue;
+                }
+                gridOverlay[coordinate.x, coordinate.y] = tr.gameObject;
+            }
+        }
+
+        static void UpdateMap(HexBehaviour hexBehaviour)
+        {
+            if (hexBehaviour == null)
+            {
+                return;
+            }
+            var position = hexBehaviour.transform.position;
+            var coordinate = CoordUtil.WorldToGrid(position);
+            gridOverlay[coordinate.x, coordinate.y] = null;
+        }
+
+        
+    }
+}
